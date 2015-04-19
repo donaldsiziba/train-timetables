0579fe856d5c5af11d375d28fd6fcc28d86a0811
donald <donaldsiziba@gmail.com>
2015-04-19 11:56:04 +0200

    1. included json builder utility class 2. externalized protocol and host

--- src/test/java/com/bddinaction/chapter2/jbehave/steps/EstimatedArrivalTimeSteps.java
+++ src/test/java/com/bddinaction/chapter2/jbehave/steps/EstimatedArrivalTimeSteps.java
@@ -1,16 +1,15 @@
 package com.bddinaction.chapter2.jbehave.steps;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.joda.JodaModule;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.apache.commons.lang.StringUtils;
+import com.bddinaction.chapter2.utilities.JsonBuilder;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.jbehave.core.annotations.*;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Named;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
 import org.joda.time.LocalTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,11 +23,19 @@ public class EstimatedArrivalTimeSteps {
     final Logger logger = LoggerFactory.getLogger(EstimatedArrivalTimeSteps.class);
     String departure;
     String destination;
+    String protocol;
+    String host;
     String port;
     String uri;
     CloseableHttpResponse response;
 
     public EstimatedArrivalTimeSteps() {
+        protocol = System.getProperty("protocol");
+        logger.info("Reading config property: protocol = {}", protocol);
+
+        host = System.getProperty("host");
+        logger.info("Reading config property: host = {}", host);
+
         port = System.getProperty("port");
         logger.info("Reading config property: port = {}", port);
     }
@@ -42,8 +49,8 @@ public class EstimatedArrivalTimeSteps {
     @Given("the next train is scheduled to leave at <departure-time> on the <line> line")
     public void givenTheNextTrainIsScheduledToLeave(@Named("departure-time") LocalTime departureTime,
                                                     String line) {
-        uri = String.format("http://localhost:%s/train-timetables/itinerary/arrivaltime/line/%s/to/%s/at/%s", port,
-                                line, destination, departureTime);
+        uri = String.format("%s://%s:%s/train-timetables/itinerary/arrivaltime/line/%s/to/%s/at/%s", protocol,host,
+                port, line, destination, departureTime);
         logger.info("URI: {}", uri);
     }
 
@@ -55,23 +62,8 @@ public class EstimatedArrivalTimeSteps {
 
     @Then("the estimated arrival time should be <arrival-time>")
     public void thenTheEstimatedArrivalTimeShouldBe(@Named("arrival-time") LocalTime expectedArrivalTime) throws IOException {
-        final Gson gson =new GsonBuilder().setPrettyPrinting().create();
-
-        final String jsonResponse = gson.toJson(EntityUtils.toString(response.getEntity()));
-        logger.info("Json Response: {}", jsonResponse);
-
-        final String strippedJsonResponse = jsonResponse.replace("\"", StringUtils.EMPTY);
-        logger.info("Stripped Json Response: {}", strippedJsonResponse);
-
-        LocalTime arrivalTime = mapper().readValue(strippedJsonResponse, LocalTime.class);
-
-        assertThat(arrivalTime).isEqualTo(expectedArrivalTime);
-    }
-
-    private ObjectMapper mapper() {
-        final ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JodaModule());
-        return mapper;
+        assertThat(new JsonBuilder().build(EntityUtils.toString(response.getEntity()), LocalTime.class))
+                .isEqualTo(expectedArrivalTime);
     }
 }
 
---
+++ src/test/java/com/bddinaction/chapter2/utilities/JsonBuilder.java
@@ -0,0 +1,50 @@
+package com.bddinaction.chapter2.utilities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by donald on 2015/04/19.
+ */
+public class JsonBuilder {
+    final Logger logger = LoggerFactory.getLogger(JsonBuilder.class);
+    private ObjectMapper mapper;
+
+    public JsonBuilder() {
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JodaModule());
+        mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setAnnotationIntrospector(new JacksonAnnotationIntrospector());
+    }
+
+    public String build(final Object object) {
+        String value = null;
+        try {
+            value = mapper.writeValueAsString(object);
+            logger.info(value);
+        } catch (JsonProcessingException e) {
+            logger.error("Exception throw... {}", e);
+        }
+        return value;
+    }
+
+    public <T> T build(final String value, Class<T> clazz) {
+        try {
+            T object = mapper.readValue(value, clazz);
+            logger.info(mapper.writeValueAsString(object));
+            return object;
+        } catch (IOException e) {
+            logger.error("Exception throw... {}", e);
+        }
+        return null;
+    }
+}
